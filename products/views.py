2f6cec557475b7a9ae695b03dcc483e26dd9e5cb
Wrexan <fwrexan@gmail.com>
2021-10-17 16:25:42 +0300

    done lesson 02

--- products/views.py
+++ products/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
+import os
+import json
+
+MODULE_DIR = os.path.dirname(__file__)
 
 # Create your views here.
 
 
 def index(request):
-    context = {'title': 'GeekShop'}
-    return render(request, 'products/index.html')
+    context = {'title': 'GeekShop111'}
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    context = {'title': 'GeekShop-Catalog'}
-    return render(request, 'products/product.html')
+    items = json.load(open(os.path.join(MODULE_DIR, 'fixtures/products.json'), encoding='utf-8'))
+    # for i in items:
+    #
+    context = {'title': 'GeekShop-Catalog',
+               'products': items}
+    return render(request, 'products/products.html', context)
