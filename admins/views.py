7c3191475f44ab2c3a4f8410127d03c3aaa418a0
Wrexan <fwrexan@gmail.com>
2021-11-04 19:39:16 +0200

    add Createview

--- admins/views.py
+++ admins/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
@@ -14,32 +15,17 @@ def index(request):
 
 
 # Create
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminRegistrationForm()
-    context = {'title': 'GeekShop - Создание пользователя', 'form': form}
-    return render(request, 'admins/admin-users-create.html', context)
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserAdminRegistrationForm
+    success_url = reverse_lazy('admins:admin_users')
+    template_name = 'admins/admin-users-create.html'
 
 
 # Read
 class UserListView(ListView):
     model = User
     template_name = 'admins/admin-users-read.html'
-
-
-# @user_passes_test(lambda u: u.is_staff)
-# def admin_users(request):
-#     context = {
-#         'title': 'GeekShop - Пользователи',
-#         'users': User.objects.all(),
-#     }
-#     return render(request, 'admins/admin-users-read.html', context)
 
 
 # Update
@@ -67,3 +53,23 @@ def admin_users_delete(request, id):
     user = User.objects.get(id=id)
     user.safe_delete()
     return HttpResponseRedirect(reverse('admins:admin_users'))
+
+# @user_passes_test(lambda u: u.is_staff)
+# def admin_users(request):
+#     context = {
+#         'title': 'GeekShop - Пользователи',
+#         'users': User.objects.all(),
+#     }
+#     return render(request, 'admins/admin-users-read.html', context)
+
+# @user_passes_test(lambda u: u.is_staff)
+# def admin_users_create(request):
+#     if request.method == 'POST':
+#         form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('admins:admin_users'))
+#     else:
+#         form = UserAdminRegistrationForm()
+#     context = {'title': 'GeekShop - Создание пользователя', 'form': form}
+#     return render(request, 'admins/admin-users-create.html', context)
