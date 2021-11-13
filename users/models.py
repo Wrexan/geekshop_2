a91c0b7500c488087c182dd79cb39f8f4fd6df4b
Wrexan <fwrexan@gmail.com>
2021-11-14 00:22:38 +0200

    a few fixes

--- users/models.py
+++ users/models.py
@@ -41,7 +41,7 @@ class UserProfile(models.Model):
 
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
-        instance.shopuserprofile.save()
+        instance.userprofile.save()
 
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
