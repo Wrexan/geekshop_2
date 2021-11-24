361e32556af9305204452907b77e8775ef01fbe7
Wrexan <fwrexan@gmail.com>
2021-11-24 22:47:57 +0200

    not sure, what we do here

--- products/models.py
+++ products/models.py
@@ -12,10 +12,18 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=256, unique=True)
     image = models.ImageField(upload_to='products_images', blank=True, null=True)
-    description = models.TextField(max_length=256, blank=True, null=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    short_desc = models.CharField(verbose_name='краткое описание', max_length=60, blank=True,)
+    description = models.TextField(verbose_name='описание', max_length=256, blank=True, null=True)
+    price = models.DecimalField(verbose_name='цена', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    is_active = models.BooleanField(verbose_name='активен', default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.name} | {self.category}'
+
+    @staticmethod
+    def get_items():
+        return Product.objects.filter(is_active=True,  quantity__gte=1).order_by('category', 'name')
