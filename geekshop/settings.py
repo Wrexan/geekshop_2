fa1e7d6606582b6ee012c6392b3ef53e9815155d
Wrexan <fwrexan@gmail.com>
2021-12-01 23:03:54 +0200

    many little changes

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 import debug_toolbar.middleware
 import environ
@@ -98,26 +99,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'geekshop.wsgi.application'
 
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# match env('DB'):  # for python >3.9
-#     case 'SQL':
-#         DATABASES = {
-#             'default': {
-#                 'ENGINE': 'django.db.backends.sqlite3',
-#                 'NAME': BASE_DIR / 'db.sqlite3',
-#             }
-#         }
-#     case _:
-#         DATABASES = {
-#             'default': {
-#                 'NAME': 'geekshop',
-#                 'ENGINE': 'django.db.backends.postgresql',
-#                 'USER': 'postgres',
-#             }
-#         }
 
 if env('DB') == 'SQL':  # for python <=3.9
     DATABASES = {
